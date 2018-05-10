5d8b2292c51b1f115f193d37d6f7554d12dcaa51
Tim Fischer <emailtimfischer@gmail.com>
2018-05-09 22:42:15 -0600

    Added range expand and custom range feature

--- index.js
+++ index.js
@@ -1,57 +1,27 @@
-
-      
       var adjustButton = document.querySelector('#adjustButton');
       var allButtons = document.querySelector('buttons');
       var clearButton = document.querySelector('#clearButton');
+      var customRangeText = document.querySelector('#rangeText');
       var guessSubmit = document.querySelector('#guessButton');
       var lastGuessDisplay = document.querySelector('#guessReadBack');
       var lastGuessText = document.querySelector('#lastGuessText');
       var maxRange = document.querySelector('#max');
       var minRange = document.querySelector('#min');
-      var secretNumber = Math.floor(Math.random() * 101 + 1);
-      var customRangeText = document.querySelector('#rangeText');
       var resetButton = document.querySelector("#resetButton")
+      var secretNumber = Math.floor(Math.random() * 101 + 1);
       var userGuess = document.querySelector('#inputField');
+
+adjustButton.addEventListener('click', function() {
+  checkRange();
+  event.preventDefault(); 
+ 
+})
 
 
 
 clearButton.addEventListener('click', function() {
   document.querySelector("#inputField").value = "";
   clearButton.disabled = true;
-})
-
-
-
-
-function defineRange (minimum,maximum){
- 
-
-  minimum = minRange.value;
-  maximum = maxRange.value;
-  customRangeText.innerText = "Custom Range Set!"
-  var customSecretNumber = Math.floor(Math.random() * (maximum - minimum) + minimum);
-  secretNumber = customSecretNumber;
-  return secretNumber;
-}
-
-function levelUp(){
-
-  alert("You Got it! Range Increased!");
-
-  }
-
-
-
-
-
-
-
-
-
-adjustButton.addEventListener('click', function() {
-  checkRange();
-  event.preventDefault();
-  defineRange();
 })
 
 guessSubmit.addEventListener('click', function() {
@@ -63,73 +33,91 @@ guessSubmit.addEventListener('click', function() {
 
 userGuess.addEventListener('keyup', function(){
   if (userGuess.value === ""){
-    adjustButton.disabled = true;
     guessSubmit.disabled = true;
     clearButton.disabled = true;
     resetButton.disabled = true;
+    adjustButton.disabled = true;
   } else {
     clearButton.disabled = false;
     guessSubmit.disabled = false;
     resetButton.disabled = false;
-  }
-})
-
-adjustButton.addEventListener('keyup', function(){
-  var maximum = maxRange.value;
-  var minimum = minRange.value;
-  if (maximum === "" || minimum === ""){
-    adjustButton.disabled = true;
-  } else {
     adjustButton.disabled = false;
   }
 })
-
-
-
-
-
-
+  
   function checkRange(){
     var minimum = minRange.value;
     var maximum = maxRange.value;
     var topText = lastGuessText;
     if (minimum > maximum || maximum === minimum || minimum < 1 || maximum < 1) {
-      topText.innerHTML = "Invalid Range!";
+       customRangeText.innerText = "Invalid Range!";
+  } else {  defineRange();
     } 
   }
 
   function compareNum() {
-          
           var topText = lastGuessText;
           var lastGuess = userGuess.value;
           var parsedValue  = parseInt(lastGuess);
-          if (isNaN(parsedValue) || parsedValue > 100 || parsedValue < 1){
-          document.querySelector('#guessReadBack').innerHTML = "";
-          document.querySelector('#hint').innerHTML = "Invalid Guess, Try again!";           
+          var minimum = minRange.value;
+          var maximum = maxRange.value;
+          if (isNaN(parsedValue) || parsedValue > maximum || parsedValue < minimum){
+           document.querySelector('#guessReadBack').innerHTML = "";
+           document.querySelector('#hint').innerHTML = "Invalid Guess, Try Again";           
           }
-
-        else if (parsedValue > secretNumber){
-          document.querySelector('#hint').innerHTML = "Too High!";
-          topText.innerText = "Your Last Guess Was";
-      } else if (parsedValue < secretNumber){
-          document.querySelector('#hint').innerHTML = "Too Low!";
-          topText.innerText = "Your Last Guess Was";
-      } else {
-        document.querySelector('#hint').innerHTML = "Boom!";
-        topText.innerText = "You Got It!";
-        levelUp();
-       
-
-      }
+          else if (parsedValue > secretNumber){
+           document.querySelector('#hint').innerHTML = "Too High!";
+           topText.innerText = "Your Last Guess Was";
+          } else if (parsedValue < secretNumber){
+           document.querySelector('#hint').innerHTML = "Too Low!";
+           topText.innerText = "Your Last Guess Was";
+          } else {
+            document.querySelector('#hint').innerHTML = "Boom! You Got It!";
+            topText.innerText = "Range Expanded + New Number Generated!";
+            maxUp();
+            minDown();
+            levelUp();
+        }
     }
  
+function defineRange (minimum,maximum){
+  var minimum = minRange.value;
+  var maximum = maxRange.value;
+  customRangeText.innerText = "Custom Range Set!"
+  var customSecretNumber = Math.floor(Math.random() * (maximum - minimum) + minimum);
+  secretNumber = customSecretNumber;
+  return secretNumber;
+}
+ 
+function levelUp (minimum,maximum){
+  var minimum = minRange.value;
+  var maximum = maxRange.value;
+  customRangeText.innerText = "Custom Range Set!"
+  var customSecretNumber = Math.floor(Math.random() * (maximum - minimum) + minimum);
+  secretNumber = customSecretNumber;
+  return secretNumber;
+}
 
-    
+function minDown (minimum){
+  var minimum = parseInt(minRange.value);
+  if (minimum > 11){
+    var newMin = minimum - 10;
+    minRange.value = newMin;
+    return minRange;
+} else if (minimum > 1 && minimum < 10) { 
+    minRange.value = 1; 
+} else {
+    return minRange;
+}
+}
 
-
-
-
-
+function maxUp (maximum){
+  var maximum = parseInt(maxRange.value);
+  var newMax = maximum + 10;
+  maxRange.value = newMax;
+  return maxRange;
+}
+ 
 
 
 
